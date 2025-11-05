2701d23767cc3c02ceee0df7ddf6c4c4fe37864c
Kritika <skritika@umich.edu>
2025-11-04 23:12:15 -0500

    pushing to branch

---
+++ packages/shared/dist/email/mock.js
@@ -0,0 +1,7 @@
+export const MockEmailProvider = {
+    async send({ to, subject, html, customId }) {
+        const messageId = `mock_${Date.now()}_${Math.random().toString(36).slice(2, 8)}`;
+        console.log("[MockEmail] send", { to, subject, htmlLen: html.length, customId, messageId });
+        return { messageId };
+    },
+};
